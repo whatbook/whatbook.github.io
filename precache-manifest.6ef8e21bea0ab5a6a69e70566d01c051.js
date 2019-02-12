f2adbad0b1958ed9fe0791a4af2eccf7869f1da3
Ako520 <jingnanbendan@163.com>
2019-02-12 11:53:04 +0800

    使用cra打包的内容

---
+++ precache-manifest.6ef8e21bea0ab5a6a69e70566d01c051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a4a072c28f59bd373f82",
+    "url": "/static/js/main.a4a072c2.chunk.js"
+  },
+  {
+    "revision": "8c4de64e73131730b305",
+    "url": "/static/js/2.8c4de64e.chunk.js"
+  },
+  {
+    "revision": "a4a072c28f59bd373f82",
+    "url": "/static/css/main.50c800f3.chunk.css"
+  },
+  {
+    "revision": "6d6b1d84a27e154aabd979e7e2a91572",
+    "url": "/index.html"
+  }
+];
